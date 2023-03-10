0575fbde310413b6eb78c83f5e2cc7aa0881f42d
Vera <veraputerls@gmail.com>
2023-03-10 15:02:21 +0600

    add taskList

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import '@/styles/index.scss'
 import headerDate from "@/js/headerDate"
 import weekList from "@/js/weekList"
+import taskList from "@/js/taskList"
 
 const app = document.querySelector('#root')
 app.append(headerDate)
 app.append(weekList)
+app.append(taskList)
